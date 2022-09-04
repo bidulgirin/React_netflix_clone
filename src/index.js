d267d1d899c824aa2120e7001fb7bcfb50d20898
bidulgirin <de05164@naver.com>
2022-09-04 14:50:53 +0900

    #

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="process.env.PUBLIC_URL">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
